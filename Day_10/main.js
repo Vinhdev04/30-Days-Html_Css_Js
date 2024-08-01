3eadf230253624d323145b34e38bfcd1d0c6fe85
Vinhdev04 <pcv.fed@gmail.com>
2024-08-01 21:48:15 +0700

    update remove all-save data on local storage and save data when reload web P2

--- Day_10/main.js
+++ Day_10/main.js
@@ -15,6 +15,7 @@ formAction.addEventListener("submit", function (event) {
     addListTodo({
       text: inputValues,
     });
+    saveDataOnLocalStorage();
   }
   // Clear input after adding
   input.value = "";
@@ -40,17 +41,49 @@ function addListTodo(notice) {
   li.addEventListener("click", function () {
     // console.log(this);
     this.classList.toggle("disabled");
+    saveDataOnLocalStorage();
   });
 
   // Remove item on click and remove status on click icon trash
   li.querySelector("i").addEventListener("click", function () {
     // console.log(this);
     this.parentElement.remove();
+    saveDataOnLocalStorage();
 
     // Cach 2:
     // let index = tags.findIndex((item) => item.text === notice.text);
-    tags.splice(index, 1); // Remove element at index with length 1
-    getTags(); // Update data on DOM
+    // tags.splice(index, 1); // Remove element at index with length 1
+    // getTags(); // Update data on DOM
   });
   listTodo.appendChild(li); // Append li to listTodo, not notice
 }
+
+// Save data in localStorage when the user reload web
+function saveDataOnLocalStorage() {
+  let listTodoElements = document.querySelectorAll("li");
+  let saveData = [];
+
+  listTodoElements.forEach(function (item) {
+    let text = item.querySelector("span").innerText;
+    let status = item.querySelector("span").getAttribute("class");
+
+    saveData.push({
+      text,
+      status,
+    });
+  });
+
+  localStorage.setItem("listTodo", JSON.stringify(saveData));
+  // console.log(JSON.parse(localStorage.getItem("listTodo")));
+}
+
+// when user Reload web, data is saved not removed from web storage
+function initial() {
+  let data = JSON.parse(localStorage.getItem("listTodo"));
+  data.forEach(function (item) {
+    console.log(item);
+    addListTodo(item);
+  });
+}
+
+initial();
