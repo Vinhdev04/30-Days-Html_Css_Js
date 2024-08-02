a2756f73d33fc699933dc3c874f873b8ca6534f8
Vinhdev04 <pcv.fed@gmail.com>
2024-08-02 22:59:49 +0700

    upload Day 11 animation toast P1

---
+++ Day_11/main.js
@@ -0,0 +1,5 @@
+let btnSuccess = document.querySelector(".container .success ");
+let btnWarning = document.querySelector(".container  .warning ");
+let btnError = document.querySelector(".container  .error");
+
+btnSuccess.addEventListener("click", () => {});
